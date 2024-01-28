67370cd1d0cd15b6b762396eee8beeba32d958c7
AngelinaShustikova <117941499+AngelinaShustikova@users.noreply.github.com>
2024-01-28 23:42:00 +0500

    init

---
+++ game.py
@@ -0,0 +1,14 @@
+"""Игра угадай число"""
+
+import numpy as np
+number = np.random.randint(1, 101)
+count = 0
+while True:
+    count += 1
+    predict_number = int()
+    if predict_number > number:
+        print('Число должно быть меньше!')
+    elif predict_number < number:
+        print('Число должно быть больше!')
+    else:
+        print('Вы угадали число! Это число = {}, за {} попыток'.format(number, count))
